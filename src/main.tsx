148f26928e53cf070ebb9f9f91f561b85761c40d
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2022-09-15 12:01:51 +0900

    Add ReloadPrompt component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
+import ReloadPrompt from "./components/ReloadPrompt/ReloadPrompt";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <ReloadPrompt />
   </React.StrictMode>
 );
