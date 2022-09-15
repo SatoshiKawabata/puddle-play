898ff43b56603ab031890e38810f5484430058cb
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2022-09-15 11:49:38 +0900

    add PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,24 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
+      manifest: {
+        name: "Puddle Play",
+        short_name: "PuddlePlay",
+        description: "Puddle Play",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "puddle-play_512x512.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "puddle-play_512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
 });
